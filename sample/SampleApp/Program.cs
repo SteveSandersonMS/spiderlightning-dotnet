c1973024797cb01cf3856edf92e0912f42a4e984
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2023-05-13 22:23:16 +0100

    Build a wasm file with a spiderlight import

---
+++ sample/SampleApp/Program.cs
@@ -0,0 +1,4 @@
+﻿using SpiderLightning;
+
+Console.WriteLine("Hello, World!");
+KeyValue.Open(1, 2, 3);
---
+++ src/SpiderLightning/KeyValue.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SpiderLightning;
+
+public partial class KeyValue
+{
+    [LibraryImport("libSystem.Native", EntryPoint = "__wasm_import_keyvalue_keyvalue_open")]
+    public static partial void Open(int a, int b, int c);
+}
